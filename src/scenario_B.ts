476ae256c8b11ab6fc726fcb7d29623cd1587d6d
Kyusaor <kyu.chrysaor@gmail.com>
2025-04-23 09:09:29 +0200

    base structure scenario B

--- src/scenario_B.ts
+++ src/scenario_B.ts
@@ -1,1 +1,24 @@
 //composite
+
+export class Employe {
+    constructor() {
+
+    }
+}
+
+
+export class Departement {
+    private _nom: string;
+    private _dependance: Departement;
+    private _employes: Employe[];
+
+    constructor(nom: string, dependance: Departement) {
+    }
+
+    //Getters / Setters
+
+    ajoutEmploye(employe: Employe) {};
+
+    retraitEmploye(employe: Employe) {};
+}
+
