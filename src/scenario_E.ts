dc545e61bf73d6d2e2a8a4e4d2b6f86f92b40495
Kyusaor <kyu.chrysaor@gmail.com>
2025-04-23 10:46:08 +0200

    ajout scenario E

--- src/scenario_E.ts
+++ src/scenario_E.ts
@@ -1,1 +1,53 @@
 //factory
+interface Personnage {
+    attaquer();
+}
+
+class PersoFactory {
+
+    static creerPerso(type: string): Personnage {
+        switch(type) {
+            case 'guerrier':
+                return new Guerrier();
+                break;
+
+            case 'mage':
+                return new Mage();
+                break;
+
+            case 'archer':
+                return new Archer();
+                break;
+
+            default:
+                throw new Error("Personnage inconnu");
+                break;
+        }
+    }
+}
+
+class Guerrier implements Personnage {
+    attaquer() {
+        console.log("LEROOOOY JENKINS")
+    }
+
+}
+
+class Mage implements Personnage {
+    attaquer() {
+        console.log("4 MILLIONS DE POINTS POUR GRYFFONDOR")
+    }
+}
+
+class Archer implements Personnage {
+    attaquer() {
+        console.log("Vous inquiétez pas, je suis nyctalope!")
+    }
+}
+
+const archer = PersoFactory.creerPerso('archer');
+const guerrier = PersoFactory.creerPerso('guerrier');
+const mage = PersoFactory.creerPerso('mage');
+archer.attaquer();
+mage.attaquer();
+guerrier.attaquer();
