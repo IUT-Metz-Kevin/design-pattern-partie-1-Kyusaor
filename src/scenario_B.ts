9f922c7013290465d2fc97a8edcdba942f0f68ac
Kyusaor <kyu.chrysaor@gmail.com>
2025-04-23 09:26:04 +0200

    etoffage classes scenar B

--- src/scenario_B.ts
+++ src/scenario_B.ts
@@ -1,24 +1,82 @@
 //composite
-
 export class Employe {
+    private _identifiant: string;
+    
     constructor() {
 
     }
-}
 
+    get identifiant() {
+        return this._identifiant;
+    }
+}
 
 export class Departement {
     private _nom: string;
-    private _dependance: Departement;
-    private _employes: Employe[];
+    private _dependance?: Departement;
+    private _employes: string[];
 
-    constructor(nom: string, dependance: Departement) {
+    constructor(nom: string, dependance?: Departement) {
+        this.nom = nom;
+        if(dependance)
+            this.dependance = dependance;
     }
 
-    //Getters / Setters
+    get nom() {
+        return this._nom;
+    }
 
-    ajoutEmploye(employe: Employe) {};
+    set nom(nom: string) {
+        this._nom = nom;
+    }
 
-    retraitEmploye(employe: Employe) {};
+    get dependance(): Departement | undefined {
+        return this._dependance;
+    }
+
+    set dependance(dpt: Departement) {
+        this._dependance = dpt;
+    }
+
+    ajoutEmploye(employe: Employe) {
+        this._employes.push(employe.identifiant);
+    };
+
+    retraitEmploye(employe: Employe) {
+        const index = this._employes.indexOf(employe.identifiant);
+        if(index >= 0)
+            this._employes.splice(index, 1);
+        else
+            throw new Error("Employé non affilié au département");
+    };
 }
+
+const SG = new Departement("Secrétariat général");
+
+
+const DT = new Departement("Departement technique");
+
+const it = new Departement("IT");
+const web = new Departement("Web");
+it.dependance = DT;
+web.dependance = DT;
+
+
+const DC = new Departement("Département commercial");
+
+const ventes = new Departement("Ventes");
+const achats = new Departement("Achats");
+ventes.dependance = DC;
+achats.dependance = DC;
+
+
+const DF = new Departement("Département financier");
+
+const rh = new Departement("RH");
+const compta = new Departement("Comptabilité");
+const admin = new Departement("Administration");
+rh.dependance = DF;
+compta.dependance = DF;
+admin.dependance = DF;
+
 
